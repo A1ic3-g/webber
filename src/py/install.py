70279d561ffda876543d8fac3766d197649f34c8
= <psyag8@exmail.nottingham.ac.uk>
2022-02-10 15:51:16 +0000

    Adjust game server path

--- src/py/install.py
+++ src/py/install.py
@@ -12,7 +12,7 @@ else:
     PATH_SEPERATOR = '/'
     
 
-INSTALL_DIR = "..{}gameservers{}".format(PATH_SEPERATOR, PATH_SEPERATOR)
+INSTALL_DIR = "..{}..{}gameservers{}".format(PATH_SEPERATOR, PATH_SEPERATOR, PATH_SEPERATOR)
 
 
 # Returns 1 if steamcmd not installed and on path else returns 0
