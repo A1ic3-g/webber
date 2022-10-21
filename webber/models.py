20b697e86c7041021926171ede80942d8743af4a
Alice Gowler <alicegowler@outlook.com>
2022-10-21 22:58:22 +0100

    steam_funcs

--- webber/models.py
+++ webber/models.py
@@ -1,5 +1,7 @@
 from flask_login import UserMixin
 from . import db
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
 class User(UserMixin, db.Model):
     # Primary key
@@ -10,3 +12,14 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     #name
     name = db.Column(db.String(1000))
+
+Declarative_base = declarative_base()
+
+class Mod(Declarative_base):
+    __tablename__ = "mod"
+
+    # Primary key
+    mod_id = Column(Integer, primary_key=True)
+    # mod symlink name
+    mod_name = Column(String, unique=True)
+
